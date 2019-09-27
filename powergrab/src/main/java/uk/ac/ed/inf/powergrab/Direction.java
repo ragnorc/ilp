78b1a48a677f091ddd1c2b4bd49c5aae02094620
Ragnor Comerford <hello@ragnor.co>
2019-09-27 16:53:58 +0100

    Add Junit and fix bugs.

--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/Direction.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/Direction.java
@@ -3,7 +3,8 @@ package uk.ac.ed.inf.powergrab;
 
 public enum Direction 
 {
-	N, NNE,  NE,  ENE, E, ESE, SE, SSE, S, SSW, SW, WSW, W, WNW, NW, NNW}
+	N, NNE,  NE,  ENE, E, ESE, SE, SSE, S, SSW, SW, WSW, W, WNW, NW, NNW;
+	
 	
 	
     public static void main( String[] args )
--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/Position.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/Position.java
@@ -10,13 +10,18 @@ public Position(double latitude, double longitude) {
 }
 public Position nextPosition(Direction direction) {
 	   double r = 0.0003;
-	   double degree = direction.ordinal() * 22.5;
-	   double newLong = this.longitude + r * Math.cos(90 - degree);
-	   double newLat = this.latitude + r * Math.sin(90 - degree);
+	   double degree = 90 - direction.ordinal() * 22.5;
+	   double radian = Math.toRadians(degree);
+	   double newLong = this.longitude + r * Math.cos(radian);
+	   double newLat = this.latitude + r * Math.sin(radian);
 	   
 	   return new Position(newLat, newLong);
 	
 }
-//public boolean inPlayArea() {  }
+public boolean inPlayArea() {
+	
+return this.latitude < 55.946233 && this.latitude > 55.942617 && this.longitude < -3.184319 && this.longitude > -3.192473;
+
+}
 
 }
